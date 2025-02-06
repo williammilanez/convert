ec44fca5fea8b86f9fc8cab455977cac5aba29ec
William Milanez <william.milanez@outlook.com>
2025-02-06 16:02:14 -0300

    identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dia.
+const USD = 5.755
+const EUR = 5.978
+const GBP = 7.163
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -13,5 +18,20 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log(currency.value)
+    switch(currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
