714a26bde5b0c68dd359a7b95c00c54fcb601afa
William Milanez <william.milanez@outlook.com>
2025-02-06 15:41:41 -0300

    limitando o input para aceitar apenas números

--- scripts.js
+++ scripts.js
@@ -2,5 +2,6 @@ const amount = document.getElementById("amount")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-    console.log(amount.value)
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
 })
